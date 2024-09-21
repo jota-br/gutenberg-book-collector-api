bb4a163687df28f1c93193a3dee0d74fc57e1433
João Carlos Roveda Ostrovski <ojcroveda@gmail.com>
2024-09-21 13:00:04 -0300

    docker

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const { mongoConnect } = require('./services/mongo');
 const PORT = process.env.PORT || 3000;
 
 const server = https.createServer({
-    key: fs.readFileSync('../key.pem'),
-    cert: fs.readFileSync('../cert.pem'),
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
 }, app);
 
 async function startServer() {
